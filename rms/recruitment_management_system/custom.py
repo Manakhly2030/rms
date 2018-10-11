0d5a455e901bc9e33adc4ad6a2b04aa44bbe531a
hereabdulla <hereabdulla@gmail.com>
2018-10-11 23:52:22 +0530

    updated Invalid URL Exception

--- rms/recruitment_management_system/custom.py
+++ rms/recruitment_management_system/custom.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 import requests
+from frappe import _
 
 
 @frappe.whitelist()
@@ -12,24 +13,29 @@ def capture_fp(name,localip):
     jsondata = {'Quality': '', 'Timeout': ''}
     url = 'http://'+localip+':8004/mfs100/capture'
     frappe.errprint(url)
-    r = requests.post(
-        url, data=jsondata,timeout=5)
-    status = r.json()
-    if 'IsoTemplate' in status:
-        return status['IsoTemplate'], status['BitmapData']
-
-
+    try:
+        r = requests.post(
+            url, data=jsondata,timeout=5)
+        status = r.json()
+        if 'IsoTemplate' in status:
+            return status['IsoTemplate'], status['BitmapData']
+    except requests.exceptions.InvalidURL,e:
+        frappe.msgprint(_("Kindly check VPN"))
+        
 @frappe.whitelist()
 def verify_fp(fp,localip):
     url = 'http://'+localip+':8004/mfs100/match' 
     frappe.errprint(url)   
     jsondata = {'BioType': 'FMR', 'GalleryTemplate': fp}
-    r = requests.post(url, data=jsondata)
-    status = r.json()
-    # if not status['ErrorCode'] == '-1307':
-    if 'Status' in status:
-        return 'Verified'
-    else:
-        return 'Not Verified'
-    # else:
-    #     return 'MFS 100 Not Found'
+    try:
+        r = requests.post(url, data=jsondata)
+        status = r.json()
+        # if not status['ErrorCode'] == '-1307':
+        if 'Status' in status:
+            return 'Verified'
+        else:
+            return 'Not Verified'
+        # else:
+        #     return 'MFS 100 Not Found'
+    except requests.exceptions.InvalidURL,e:
+        frappe.msgprint(_("Kindly check VPN"))
